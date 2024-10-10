01355be90e02a88c7925f0d337a54694202c6217
KuybinaE <kijbuna1234@gmail.com>
2024-10-10 15:19:00 +0500

    10 октября

--- src/Tkinter/admin_panel.py
+++ src/Tkinter/admin_panel.py
@@ -1,8 +1,22 @@
 #Окно-панель администратора
 from tkinter import *
+
+from PyQt5.QtMultimedia.QMediaRecorder import status
+
 from src.Controllers.UserController import UserController
+from src.Tkinter.login import title
 
 
+#Функционал
+#Создание диалогового окна
+def dialog_window_update_status(id_login):
+    dialog_window = Tk()
+    dialog_window,title("Подтверждение увольнения")
+    dialog_window.geometry
+def click_test():
+    button_login.configure(text="Уволен",background='green')
+
+#Отрисовка
 admin_panel = Tk()
 admin_panel.geometry('1700x800')
 admin_panel.title("Панель администратора")
@@ -20,18 +34,22 @@ user = UserController()
 list_user = user.get()
 count_row = 2
 for row in list_user:
-    login_title = Label(admin_panel,text=row.login)
-    login_title.grid(column=0,row=count_row,padx=1,pady=1)
-    login_role = Label(admin_panel,text=row.role_id.role)
-    login_role.grid(column=1,row=count_row,padx=0,pady=1)
-    button_login=Button(admin_panel,
-                        text='Уволить',
-                        height=2,
-                        width=8,
-                        background='red',
-                        foreground='white',
-                        command=user.update_status(row.id))
-    button_login.grid(column=2,row=count_row,padx=0,pady=1)
+    if row.status:
+        login_title = Label(admin_panel,text=row.login)
+        login_title.grid(column=0,row=count_row,padx=1,pady=1)
+        login_role = Label(admin_panel,text=row.role_id.role)
+        login_role.grid(column=1,row=count_row,padx=0,pady=1)
+        button_login=Button(admin_panel,
+                            text='Уволить',
+                            height=2,
+                            width=8,
+                            background='red',
+                            foreground='white',
+                            command=user.update_status(row.id))
+        status_title = Label(admin_panel,text=row.id)
+        status_title.grid
+        button_login.grid(column=2,row=count_row,padx=0,pady=1)
+    print(row.login,row.status)
     count_row+=1
 
 
