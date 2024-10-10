cc43836eb4c04e80dc7b9b1b7311f918613de21a
KuybinaE <kijbuna1234@gmail.com>
2024-10-10 14:16:13 +0500

    10 октября

---
+++ src/Tkinter/cook_test.py
@@ -0,0 +1,13 @@
+# from tkinter import *
+#
+# window = Tk() #вызываем класс tkinter
+# window.title("Информационная система КАФЕ") #создаем название окна
+# window.geometry('700x600')
+# title = Label(window,text="Панель повара",font=("Times New Roman",16))
+#
+# title.grid(column = 0, row = 0)#координаты текста в окне
+
+
+
+
+# window.mainloop() #запускаем окно
